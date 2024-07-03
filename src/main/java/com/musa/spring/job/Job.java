b9190591f787e6abba8931f8235595b73fe8c0ff
Ibrahim Musa <ibrahimmusa@imusa-sg.local>
2024-07-03 17:22:50 +0100

    updated

---
+++ src/main/java/com/musa/spring/job/Job.java
@@ -0,0 +1,34 @@
+package com.musa.spring.job;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+//@Table(name = "job_table")
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private String minSalary;
+    private String maxSalary;
+    private String location;
+
+    public Job() {
+    }
+
+    public Job(Long id, String title, String description, String minSalary, String maxSalary, String location) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        this.location = location;
+    }
+}
---
+++ src/main/java/com/musa/spring/job/JobRepository.java
@@ -0,0 +1,10 @@
+package com.musa.spring.job;
+
+
+import com.musa.spring.job.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long> {
+}
